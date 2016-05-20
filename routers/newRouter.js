025c3a3bee88fdcabb095b415e01e8429b365d9d
dylanksys <j:null>
2016-05-19 20:00:25 -0700

    Initial Commit

---
+++ routers/newRouter.js
@@ -0,0 +1,9 @@
+var router = require('express').Router();
+
+// Change API methods and endpoint methods to fit your needs
+
+router.get('/myNewEndpoint', function(req, res) {
+  res.send('Path: /myNewEndpoint');
+};
+
+module.exports = router;
